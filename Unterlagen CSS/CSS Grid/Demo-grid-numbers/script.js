0e627fa4affad93ab92df229ae959f27a04c998e
Georg Graf (SPG) <grafg@spengergasse.at>
2024-10-22 06:57:15 +0200

    even more

--- Unterlagen CSS/CSS Grid/Demo-grid-numbers/script.js
+++
@@ -1,6 +0,0 @@
-const par = document.getElementById("size-output");
-function logViewPortSize() {
-    par.innerHTML = `${window.innerWidth}px * ${window.innerHeight}px`;
-}
-window.addEventListener("resize", logViewPortSize);
-logViewPortSize();
