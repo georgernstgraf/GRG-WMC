59c97e2524396c0f0664f34101427479faabb56a
Georg Graf (SPG) <grafg@spengergasse.at>
2025-02-24 17:34:03 +0100

    static

---
+++ HONO-DENO/main.js
@@ -0,0 +1,9 @@
+import { Hono } from "hono";
+import { serveStatic } from "hono/deno";
+
+const app = new Hono();
+
+app.get("/", serveStatic({ path: "./static/index.html" }));
+app.get("*", serveStatic({ root: "./static" }));
+
+Deno.serve(app.fetch);
