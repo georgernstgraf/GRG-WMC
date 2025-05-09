d82668a259a292f9a75b3098c9086d90c1fd24c6
Georg Graf (SPG) <grafg@spengergasse.at>
2025-05-09 19:26:51 +0200

    it lives in SWP

--- 1HWII_PLF4_Quiz_node_only_euler/ANG/plf.js
+++
@@ -1,3 +0,0 @@
-const Frage = undefined;  // Aufgabe 1, wird Klasse
-const eulerZahl = undefined;  // Aufgabe 2, wird Function
-module.exports = { Frage, eulerZahl };  // bleibt am Ende des Programms
