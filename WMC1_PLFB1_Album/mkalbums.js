27f6ef566b04b345684191433978e901977ffb2a
Georg Graf (SPG) <grafg@spengergasse.at>
2025-05-29 20:54:22 +0200

    habe alben

---
+++ WMC1_PLFB1_Album/mkalbums.js
@@ -0,0 +1,19 @@
+import { faker } from "@faker-js/faker";
+
+const alben = Array.from({ length: 5 }).map((_) => {
+    const rv = {
+        album: faker.music.album(),
+        artist: faker.music.artist(),
+        genre: faker.music.genre(),
+        year: faker.date.past(20).getFullYear(),
+        songs: Array.from({ length: faker.number.int({ min: 1, max: 5 }) })
+            .map(() => ({
+                title: faker.music.songName(),
+                duration: faker.number.int({ min: 50, max: 700 }), // Duration in seconds
+            })),
+    };
+    rv.songs.sort(() => Math.random() - 0.5);
+    return rv;
+});
+
+console.log("export const alben = " + JSON.stringify(alben, null, 4));
