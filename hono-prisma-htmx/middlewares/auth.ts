67014f486f2fcebd579147c2329d45cba02f18f4
Georg Graf (SPG) <grafg@spengergasse.at>
2025-06-01 14:31:28 +0200

    renames #2

--- hono-prisma-htmx/middlewares/auth.ts
+++ hono-prisma-htmx/middlewares/auth.ts
@@ -1,8 +1,8 @@
-import { Context, Next } from 'https://deno.land/x/hono/mod.ts';
+import { Context, Next } from "hono";
 
 export const authMiddleware = async (c: Context, next: Next) => {
-  const userRole = c.req.header('x-user-role'); // Replace with actual auth logic
-  if (!userRole) return c.text('Unauthorized', 401);
-  c.set('userRole', userRole);
+  const userRole = c.req.header("x-user-role"); // Replace with actual auth logic
+  if (!userRole) return c.text("Unauthorized", 401);
+  c.set("userRole", userRole);
   await next();
 };
