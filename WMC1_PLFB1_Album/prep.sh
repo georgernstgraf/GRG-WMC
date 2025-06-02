30f357f08a7721a4ed85b39fa5bd4e3e49ac814b
Georg Graf (SPG) <grafg@spengergasse.at>
2025-06-02 21:01:28 +0200

    prep

--- WMC1_PLFB1_Album/prep.sh
+++ WMC1_PLFB1_Album/prep.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 FILES_TO_EMPTY="plf.js"
 FILES_TO_RM="songs.json mkalbums.js deno.lock clean.sh README.* .gitignore"
-for i in plf.js; do
+for i in $FILES_TO_EMPTY; do
     echo >$i
 done
-echo "Now copy this folder and GIT RESET --HARD # git reset --hard"
+for i in for i in $FILES_TO_RM; do
+    rm -f $i
+done
